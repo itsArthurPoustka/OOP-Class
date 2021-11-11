e97e5e58638edcf390d7aaa45e6bcba9e538a29d
itsArthurPoustka <84039665+itsArthurPoustka@users.noreply.github.com>
2021-11-11 15:55:02 -0300

    Delete LuizAndre.java

--- src/main/java/uff/ic/lleme/tcc00328/s20212/exercicio/exercicio2/LuizAndre.java
+++
@@ -1,8 +0,0 @@
-package uff.ic.lleme.tcc00328.s20212.exercicio.exercicio2;
-
-public class LuizAndre {
-
-    public static void main(String[] args) {
-        System.out.println("Alô mundo!!!!!!!!!");
-    }
-}
