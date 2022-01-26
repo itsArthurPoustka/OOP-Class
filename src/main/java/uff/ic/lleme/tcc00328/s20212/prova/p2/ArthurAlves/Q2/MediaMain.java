167e9444e8a12f70f281d7d966227aa993f2783a
itsArthurPoustka <84039665+itsArthurPoustka@users.noreply.github.com>
2022-01-26 12:19:11 -0300

    Create MediaMain.java

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20212/prova/p2/ArthurAlves/Q2/MediaMain.java
@@ -0,0 +1,29 @@
+package uff.ic.lleme.tcc00328.s20212.prova.p2.ArthurAlves.Q2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
+public class MediaMain {
+    
+    public static double somaAcumulada = 0f;
+    
+    public static void main(String[] args){
+            
+            List<Double> valores = new ArrayList<>();
+
+            for(int i = 0; i < 100000; i++){
+                valores.add(Math.random() * 11);
+            }
+
+            ForkMean fb = new ForkMean(valores, somaAcumulada);
+            ForkJoinPool pool = new ForkJoinPool();
+
+            pool.invoke(fb);
+            
+            System.out.println("A média calculada é: " + ForkMean.getSomaAtual()/valores.size());
+
+        }
+    
+}
+
