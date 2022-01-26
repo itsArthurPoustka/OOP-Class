51f684072a7f4f2103da605990c9a9beabc8b30a
itsArthurPoustka <84039665+itsArthurPoustka@users.noreply.github.com>
2022-01-26 12:17:54 -0300

    Create Substantivo.java

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20212/prova/p2/ArthurAlves/Q1/Substantivo.java
@@ -0,0 +1,7 @@
+package uff.ic.lleme.tcc00328.s20212.prova.p2.ArthurAlves.Q1;
+
+public class Substantivo extends Sinonimos {
+    public Substantivo(String significado){
+        super(significado);
+    }
+}
